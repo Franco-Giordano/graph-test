a3fc64242960fb8920e027f571e5dbfe9dbedf34
franco-giordano <francogior98@gmail.com>
2020-02-09 22:37:53 -0300

    cambios en colores

--- src/app/graph/graph.page.ts
+++ src/app/graph/graph.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.page.html',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphPage implements OnInit {
 
-  constructor() { }
+
+  barData = [
+    { season: 'S1', viewers: 2500000 },
+    { season: 'S2', viewers: 3800000 },
+    { season: 'S3', viewers: 5000000 },
+    { season: 'S4', viewers: 6900000 },
+    { season: 'S5', viewers: 6900000 },
+    { season: 'S6', viewers: 7500000 },
+    { season: 'S7', viewers: 10000000 },
+    { season: 'S8', viewers: 17000000 }
+  ];
+  title = 'Game of Thrones';
+  subtitle = 'Viewers per season for';
+  width: number;
+  height: number;
+  margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  x: any;
+  y: any;
+  svg: any;
+  g: any;
+
+  constructor() {
+    this.width = 900 - this.margin.left - this.margin.right;
+    this.height = 500 - this.margin.top - this.margin.bottom;
+  }
 
   ngOnInit() {
+
+    functionPlot({
+      target: '#my-graph',
+      data: [{
+        fn: 'x^2'
+      }]
+    });
   }
+
 
 }
